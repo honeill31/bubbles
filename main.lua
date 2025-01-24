57b4cfe0447e4b1f38dd759d8906ade4f87fa8e6
honeill31 <honeill31@gmail.com>
2025-01-24 19:47:11 +0000

    made some chabges

--- main.lua
+++ main.lua
@@ -1,6 +1,16 @@
-function love.draw()
-    love.graphics.print("By Maggie and Holly", 400,200)
+
+local main_text
+
+function draw_main_screen()
     love.graphics.print("Welcome to the bubbles game!", 400, 300)
+    love.graphics.print("By Maggie and Holly", 400,400)
     love.graphics.circle("fill", 10, 10, 100, 25)
     love.graphics.rectangle("line", 200, 30, 120, 100)
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    draw_main_screen()
 end
