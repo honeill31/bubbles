0db9bb3e97b2dee1cd6d1d5405ca5bf98b33ef4d
honeill31 <honeill31@gmail.com>
2025-01-24 18:07:02 +0000

    lovely start up screen

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+    love.graphics.print("By Maggie and Holly", 400,200)
+    love.graphics.print("Welcome to the bubbles game!", 400, 300)
+    love.graphics.circle("fill", 10, 10, 100, 25)
+    love.graphics.rectangle("line", 200, 30, 120, 100)
 end
