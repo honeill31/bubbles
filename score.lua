fec87a16effe1b7d9126434d924b9222f536bf93
honeill31 <honeill31@gmail.com>
2025-01-25 11:17:26 +0000

    temp score

--- score.lua
+++ score.lua
@@ -2,3 +2,16 @@
 function increase_score(sc)
     sc = sc + 1
 end
+
+function can_increase_score(circleX)
+    left_rectangle = {
+        width = 300,
+        height = 300,
+        x = 10,
+        y = 400
+    }
+    
+    (10, 400, 300, 300)
+    right_rectangle = (1290, 400, 300, 300)
+    if ()
+end
