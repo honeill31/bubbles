014273eb7c753b03f94aa92b9a948276568139c7
opiaa <niesypiam666@gmail.com>
2025-01-25 15:56:30 +0000

    colour update

--- colour.lua
+++ colour.lua
@@ -1,14 +1,36 @@
 left_rectangle_colour = {
-    red = 31,
-    green = 168,
-    blue = 153,
+    red = 195,  -- From #c36272
+    green = 98,
+    blue = 114,
     alpha = 255
 }
 
 right_rectangle_colour = {
-    red = 118,
-    green = 179,
-    blue = 208,
+    red = 244,  -- From #f4cc86
+    green = 204,
+    blue = 134,
+    alpha = 255
+}
+
+
+ball_1 = {
+    red = 90,   -- From #5a9374
+    green = 147,
+    blue = 116,
+    alpha = 255
+}
+
+ball_2 = {
+    red = 52,   -- From #347dab
+    green = 125,
+    blue = 171,
+    alpha = 255
+}
+
+ball_3 = {
+    red = 144,  -- From #9086ba
+    green = 134,
+    blue = 186,
     alpha = 255
 }
 
@@ -16,26 +38,5 @@ background_colour = {
     red = 247,
     green = 241,
     blue = 225,
-    alpha = 255
-}
-
-ball_1 = {
-    red = 255,
-    green = 0,
-    blue = 0,
-    alpha = 255
-}
-
-ball_2 = {
-    red = 0,
-    green = 0,
-    blue = 0,
-    alpha = 255
-}
-
-ball_3 = {
-    red = 255,
-    green = 0,
-    blue = 0,
     alpha = 255
 }
