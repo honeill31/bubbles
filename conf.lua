fd20edcaa7e186d0f2fb4b83cf0912c132c1af5f
honeill31 <honeill31@gmail.com>
2025-01-25 10:33:48 +0000

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-    t.window.width = 1600  -- Set the width of the window
+    t.window.width = 1400  -- Set the width of the window
     t.window.height = 600 -- Set the height of the window
     t.window.title = "My Game" -- Set the window title
 end
